c479e30f31b6e08f7018e3a4ee117f18f624bb29
Lukas <lukas.dufek@tul.cz>
2022-10-04 13:40:09 +0200

    NameDayApp

---
+++ svatek/svatek/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace svatek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDownDay = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownMonth = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMonth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownDay
+            // 
+            this.numericUpDownDay.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownDay.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDownDay.Location = new System.Drawing.Point(391, 348);
+            this.numericUpDownDay.Name = "numericUpDownDay";
+            this.numericUpDownDay.Size = new System.Drawing.Size(190, 56);
+            this.numericUpDownDay.TabIndex = 0;
+            this.numericUpDownDay.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // numericUpDownMonth
+            // 
+            this.numericUpDownMonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDownMonth.Location = new System.Drawing.Point(391, 439);
+            this.numericUpDownMonth.Name = "numericUpDownMonth";
+            this.numericUpDownMonth.Size = new System.Drawing.Size(190, 56);
+            this.numericUpDownMonth.TabIndex = 1;
+            this.numericUpDownMonth.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 336);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 59);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Den:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 439);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 59);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Měsíc:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(115, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(555, 134);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Dnešek!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(12, 611);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(616, 62);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Dnes má svátek: ";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lime;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(646, 579);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 133);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "OK";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrchid;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(796, 724);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDownMonth);
+            this.Controls.Add(this.numericUpDownDay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMonth)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDownDay;
+        private System.Windows.Forms.NumericUpDown numericUpDownMonth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ svatek/svatek/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace svatek
+{
+    
+    public partial class Form1 : Form
+    {
+        public static List<string> jmena;  
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime ted = DateTime.Now;
+            int mesic = ted.Month;
+            int den = ted.Day;
+
+            numericUpDownMonth.Value = mesic;
+            numericUpDownDay.Value = den;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int hledany_den = Decimal.ToInt32(numericUpDownDay.Value);
+            int hledany_mesic = Decimal.ToInt32(numericUpDownMonth.Value);
+
+           textBox1.Text= "Dnes má svátek: " + najdiPodleDatumu(hledany_den, hledany_mesic);
+        }
+
+
+        public static String najdiPodleDatumu(int hledany_den, int hledany_mesic)
+        {
+
+            jmena = new List<string>();
+            using (StreamReader sr = new StreamReader(@"jmena.txt"))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    jmena.Add(s);
+                }
+            }
+
+            String jmeno = "";
+            int den = 0;
+            int mesic = 1;
+
+
+            for (int i = 0; i < jmena.Count; i++)
+            {
+                den++;
+                if ((hledany_den == den) && (hledany_mesic == mesic))
+                {
+                    jmeno = jmena[i];
+                }
+                else if (String.IsNullOrEmpty(jmena[i]))
+                {
+                    den = 0;
+                    mesic++;
+                }
+            }
+
+
+
+            return jmeno;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /*
+        public void pridejJmena()
+        {
+            jmena = new List<string>();
+            using (StreamReader sr = new StreamReader(@"jmena.txt"))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    jmena.Add(s);
+                }
+            }
+            
+        }
+        */
+
+    }
+}
